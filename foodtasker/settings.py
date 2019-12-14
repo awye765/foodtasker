4a84b8847819c4881992fd9b65f912ac0ee16048
awye765 <awye765@googlemail.com>
2019-12-14 19:26:51 +0000

    Deploy - fixed error in requirements 8.txt

--- foodtasker/settings.py
+++ foodtasker/settings.py
@@ -129,6 +129,6 @@ MEDIA_URL = '/media/'
 
 import dj_database_url
 db_from_env = dj_database_url.config()
-DATABASE['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 # foodie12345678
