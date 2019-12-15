a0a825d5b4692437c275c25d49cca5b6664dbdc4
awye765 <awye765@googlemail.com>
2019-12-15 18:27:25 +0000

    Cannot get order details to render:

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from foodtaskerapp.forms import UserForm, RestaurantForm, UserFormForEdit, MealForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from foodtaskerapp.models import Meal
+from foodtaskerapp.models import Meal, Order
 # Create your views here.
 def home(request):
     return redirect(restaurant_home)
@@ -81,7 +81,8 @@ def restaurant_edit_meal(request, meal_id):
 @login_required(login_url='/restaurant/sign-in/')
 
 def restaurant_order(request):
-    return render(request, 'restaurant/order.html', {})
+    orders = Order.objects.filter(restaurant = request.user.restaurant).order_by("-id")
+    return render(request, 'restaurant/order.html', {"orders": orders})
 
 # Restaurant Report
 
