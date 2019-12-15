24f96a364f09ffc7e424d9a91a4ed130a511ec3d
awye765 <awye765@googlemail.com>
2019-12-15 08:39:20 +0000

    Added Customer and Driver models

--- foodtaskerapp/admin.py
+++ foodtaskerapp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from foodtaskerapp.models import Restaurant
+from foodtaskerapp.models import Restaurant, Customer, Driver
 
 admin.site.register(Restaurant)
+admin.site.register(Customer)
+admin.site.register(Driver)
+
 # Register your models here.
