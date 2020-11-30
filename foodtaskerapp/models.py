11c81391b5132cb311244a6a7379ab754d0350b7
awye765 <awye765@googlemail.com>
2020-11-30 16:10:52 +0000

    Add sign-up view and functionality

--- foodtaskerapp/models.py
+++ foodtaskerapp/models.py
@@ -9,6 +9,7 @@ class Restaurant(models.Model):
     address = models.CharField(max_length=500)
     logo = models.ImageField(upload_to='restaurant_logo/', blank=False)
 
+    # Displays the name of the restaurant in the admin dashboard, not the ID
     def __str__(self):
         return self.name
 
--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from foodtaskerapp.forms import UserForm, RestaurantForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 # Create you views here.
 def home(request):
@@ -10,4 +13,34 @@ def restaurant_home(request):
     return render(request, 'restaurant/home.html', {})
 
 def restaurant_sign_up(request):
-    return render(request, 'restaurant/sign_up.html', {})
+    user_form = UserForm()
+    restaurant_form = RestaurantForm()
+
+    # When user hits Sign Up button, run below code
+    if request.method == "POST":
+        # Get data from the forms for each new user and restaurant
+        user_form = UserForm(request.POST)
+        restaurant_form = RestaurantForm(request.POST, request.FILES)
+
+        # If data valid
+        if user_form.is_valid() and restaurant_form.is_valid():
+            # Create new user object for restaurant owner
+            new_user = User.objects.create_user(**user_form.cleaned_data)
+            # Create new restaurant, but don't commit to db. Hold in mem only
+            new_restaurant = restaurant_form.save(commit=False)
+            # Set user of that restaurant to the new user
+            new_restaurant.user = new_user
+            # Now save restaurant to db
+            new_restaurant.save()
+
+            login(request, authenticate(
+                username = user_form.cleaned_data["username"],
+                password = user_form.cleaned_data["password"]
+            ))
+
+            return redirect(restaurant_home)
+
+    return render(request, 'restaurant/sign_up.html', {
+        "user_form": user_form,
+        "restaurant_form": restaurant_form
+    })
